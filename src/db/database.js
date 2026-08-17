c79a93931b47835635b35410a8fc7d6380db71ec
Jorge-Daniel-dev <danielsmoke1@hotmail.com>
2026-08-17 16:51:20 -0600

    Corrige conexión a PostgreSQL

--- src/db/database.js
+++ src/db/database.js
@@ -1,9 +1,12 @@
-import pkg from 'pg'
-const {Pool} = pkg;
+import pkg from 'pg';
+
+const { Pool } = pkg;
 
 const pool = new Pool({
-connectionString: process.env.postgresql://neondb_owner:npg_ok1NsyiQShw5@ep-dark-flower-aym8w4lg-pooler.c-5.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require,
-  ssl: { rejectUnauthorized: false }
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 export default pool;
